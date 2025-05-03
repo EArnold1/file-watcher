faa3537d1c7d3a891f57bf5386e9834f2e81eadd
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2025-05-04 00:21:44 +0100

    create a function to handle folders and files recursively

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,26 @@ use std::{
     time::{Duration, SystemTime},
 };
 
+struct WatcherEvent {
+    file_name: String,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = mpsc::channel::<()>();
+    let (tx, rx) = mpsc::channel::<WatcherEvent>();
 
-    let file_path = "raw.txt";
+    let file_path = ".";
 
-    // perform watch in another thread
-    // calling the watcher in the main thread will block it and the receiver will still be waiting for the function to finish executing
-    thread::spawn(|| watcher(file_path, tx));
+    recursive_file_reader(file_path, tx);
 
-    for _ in rx {
-        println!("{file_path}: changed");
+    for event in rx {
+        println!("{} changed", event.file_name);
     }
 
     Ok(())
 }
 
-fn watcher(file_path: &str, tx: mpsc::Sender<()>) -> std::io::Result<()> {
+fn watcher(path: PathBuf, tx: mpsc::Sender<WatcherEvent>) -> std::io::Result<()> {
     let mut last_modified: Option<SystemTime> = None;
-
-    let path = PathBuf::from(file_path);
 
     loop {
         let metadata = path.metadata()?;
@@ -37,10 +37,37 @@ fn watcher(file_path: &str, tx: mpsc::Sender<()>) -> std::io::Result<()> {
         let modified = metadata.modified()?;
 
         if Some(modified) != last_modified {
-            let _ = tx.send(()); // send modified signal
-            last_modified = Some(modified)
+            if let Some(file_name) = path.file_name() {
+                match file_name.to_str() {
+                    Some(file_name) => {
+                        let _ = tx.send(WatcherEvent {
+                            file_name: file_name.to_string(),
+                        }); // send modified signal
+                        last_modified = Some(modified)
+                    }
+                    None => panic!("something went wrong"),
+                }
+            }
         }
 
         thread::sleep(Duration::from_millis(300)); // check every 300 millis
+    }
+}
+
+fn recursive_file_reader(file_path: &str, tx: mpsc::Sender<WatcherEvent>) {
+    let path = PathBuf::from(file_path);
+
+    if path.is_dir() {
+        for entry in path.read_dir().expect("read_dir call failed").flatten() {
+            if let Some(file_name) = entry.path().to_str() {
+                recursive_file_reader(file_name, tx.clone())
+            };
+        }
+    } else {
+        // perform watch in another thread
+        // calling the watcher in the main thread will block it and the receiver will still be waiting for the function to finish executing
+        // thread::spawn(|| watcher(file_path, tx));
+        thread::spawn(move || watcher(path, tx));
+        // is spawning a new thread for folders with nested files/folder good ?
     }
 }
